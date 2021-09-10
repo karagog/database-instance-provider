38aae5d429f46922fffb4c8d3c4506558afd9181
George Karagoulis <karagog@gmail.com>
2021-09-10 09:49:25 -0700

    Improved the presubmit script.
    Now it runs bazel test too, along with Gazelle to make sure the BUILD
    files are up to date.

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-set -e
+set -e  # abort the script if any single command fails.
 
+# Tidy up the go modules in case anything changed there.
 go mod tidy
 
-# Make sure go test pass.
-go test ./... -count=1
+# Bring the BUILD.bazel files up to date with go module imports.
+#
+# NOTE: If you added new module imports via "go get", you may also have
+# to update the repos in WORKSPACE.bazel like so:
+#
+#  $ bazel run //:gazelle -- update-repos --from_file=go.mod
+bazel run //:gazelle
 
-# Make sure Bazel test pass.
+# Make sure go test passes.
+go test github.com/karagog/db-provider/... -count=1
+
+# Make sure Bazel test passes. The result should be the same as "go test",
+# unless the BUILD/WORKSPACE files are out of sync with the go packages.
+# See the notes above about running Gazelle to fix this.
 bazel test //... --nocache_test_results
